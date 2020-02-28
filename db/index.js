cd9f4b375cf5b57783a49a7b5bb996cfa705c2d7
Aaron Stevens <astevens@spp.org>
2020-02-28 08:08:33 -0600

    Removed the hard-coded mongo url (hope that changes itself at some point)

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(
     process.env.MONGODB_URI ||
-      "mongodb://heroku_303x97p1:75fk94evijob7qdnidqae1osue@ds061360.mlab.com:61360/heroku_303x97p1",
+      '',
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
